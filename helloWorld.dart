fbccce24456303b74b11f378f35480b00d928199
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-08-19 21:33:09 -0300

    Create helloWorld.dart

---
+++ helloWorld.dart
@@ -0,0 +1,15 @@
+void main() {
+  String nome = "Vinicius";
+  nome = "Tatiane";
+
+  String sobrenome = "Camargo";
+
+  print(nome + sobrenome);
+
+  //print de texto
+  print("teste do dart");
+
+  String? cor;
+  String resultado = cor ?? "Sem cor";
+  print(resultado);
+}
