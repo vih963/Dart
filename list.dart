131d8e186482b2f0465f6c70c40ebbdd90110efa
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-08-21 21:24:54 -0300

    dart

--- list.dart
+++ list.dart
@@ -1,9 +1,15 @@
 void main() {
-  List nome = ["Vini", 55.5, false];
-  print(nome);
+  // List nome = ["Vini", 55.5, false];
+  // print(nome);
 
+  List<String> cor = ["Azul", "Amarelo", "verde"];
+  cor.insert(0, "roxo");
+  cor.add("preto");
+  cor.removeLast();
+  // cor.removeAt(0);
 
+  print(cor.length);
+  print(cor.contains("Azul"));
 
-
-  
+  print(cor);
 }
