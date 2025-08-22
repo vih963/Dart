ebdd66624e8b6b192223af4b2a16c7d2f038674d
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-08-21 21:38:03 -0300

    Dart map

--- map.dart
+++ map.dart
@@ -1,7 +1,11 @@
-void main (){
+void main() {
+  Map<int, /*String*/ dynamic> nomes = {1: "Vinicius", 2: "lucas", 3: "Pedro"};
+  print(nomes);
 
+  // print(usuario["dev"]);
+  // usuario["idade"] = 25;
+  // print(usuario);
+  // print(usuario.length);
 
-
-
-  
+  print(nomes);
 }
