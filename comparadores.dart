2267a16a4268885641d26080ac72000c7f540281
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-08-21 21:53:32 -0300

    dart if else

---
+++ comparadores.dart
@@ -0,0 +1,18 @@
+// Comparadores
+//  > maior
+//  < menor
+//  >= maior igual
+//  <= menor igual
+//  == igual
+//  != diferent
+
+void main() {
+  var valor = "1";
+  if (valor == true) {
+    print("Valor é $valor");
+  } else if (valor == false) {
+    print("Valor é $valor");
+  } else {
+    print("o Valor não é verdadeiro e nem falso");
+  }
+}
