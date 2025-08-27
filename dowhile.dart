f6d32c980d7526845a7869ab95eadc3f1e91f0f8
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-08-26 21:18:24 -0300

    dart do while

--- dowhile.dart
+++ dowhile.dart
@@ -1,8 +1,15 @@
 void main() {
-  int valor = 5;
+  // int valor = 5;
+  repeticaoDoWhile();
+  print(somar(10, 20));
+}
 
+repeticaoDoWhile() {
+  int valor = 5;
   do {
-    print("O valor é $valor");
+    // print("O valor é $valor");
     valor++;
   } while (valor <= 10);
 }
+
+int somar(int valor1, int valor2) => valor1 + valor2;
