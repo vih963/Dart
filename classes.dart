e11ace2e23c5b3450b0e5c3bd6185da7c86ba65c
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-08-26 21:37:30 -0300

    Dart classes

---
+++ classes.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+class Animal {
+  String? tipo;
+  String? cor;
+  int? idade;
+
+  dormir() {
+    print("O animal $tipo está dormindo");
+  }
+}
+
+void main() {
+  Animal animal = new Animal();
+  animal.tipo = "cachorro";
+  animal.cor = "Caramelo";
+  animal.idade = 10;
+
+  Animal animal2 = new Animal();
+  animal2.tipo = "Gato";
+  animal2.cor = "Amarelo";
+  animal2.idade = 3;
+
+  print(animal.tipo);
+  print(animal.cor);
+  print(animal.idade);
+
+  print(animal2.tipo);
+  print(animal2.cor);
+  print(animal2.idade);
+}
